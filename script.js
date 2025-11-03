303120e321f261e6a6b5c95e4c2adbeb0017741e
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-03 14:20:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+document.getElementById("calculate").addEventListener("click", function() {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  const operator = document.getElementById("operator").value;
+  let result;
+
+  if (isNaN(num1) || isNaN(num2)) {
+    result = "Please enter valid numbers!";
+  } else {
+    switch (operator) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        result = num2 !== 0 ? num1 / num2 : "Cannot divide by zero!";
+        break;
+      default:
+        result = "Invalid operator!";
+    }
+  }
+
+  document.getElementById("result").textContent = "Result: " + result;
+});
+
+document.getElementById("change_text").addEventListener("click", function() {
+  const newTexts = ["Bye", "Good Bye", "To", "Your", "Class"];
+  const divisions = document.querySelectorAll(".division");
+  divisions.forEach((div, index) => {
+    div.textContent = newTexts[index];
+  });
+});
